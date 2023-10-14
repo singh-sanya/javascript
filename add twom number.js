bfd444616676288f9b3e687d913672e9dee631b2
singh-sanya <115203006+singh-sanya@users.noreply.github.com>
2023-10-14 13:32:53 +0530

    add two number using js

---
+++ add twom number.js
@@ -0,0 +1,8 @@
+function additionFunction(a, b) { 
+    return a + b; 
+} 
+  
+let num1 = 5; 
+let num2 = 10; 
+let sum = additionFunction(num1, num2); 
+console.log("Sum of given numbers is :", sum);
